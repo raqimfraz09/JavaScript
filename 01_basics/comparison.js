1b33673b880d9e682e5953012020c7a6983b8280
RAQIM FRAZ <79056198+raqimfraz09@users.noreply.github.com>
2024-05-30 18:56:33 +0000

    container config done

---
+++ 01_basics/comparison.js
@@ -0,0 +1,18 @@
+console.log(2 > 1)   // True
+console.log(2 >= 1)  // True
+console.log(2 < 1)  //  False
+console.log(2 <= 1) // False
+console.log(2 != 1) // True
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);   // false
+console.log(null == 0);  // false
+console.log(null >= 0); // true, in this null is converted into 0.
+
+// console.log(undefined == 0); // it gives false value in all the scenario.
+
+// === It not only check the value but also checks the data type too
+
+console.log("2" === 2);  // It gives false value.
