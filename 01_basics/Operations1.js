8f92c838e6ab027af941675f9fe2d2e9be125ff8
RAQIM FRAZ <79056198+raqimfraz09@users.noreply.github.com>
2024-05-30 18:43:54 +0000

    container config done

---
+++ 01_basics/Operations1.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let value = 3 
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+
+
+// String Operations on Numbers 
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)     // 12
+// console.log(1 + "2")     // 12
+// console.log("1" + 2 + 2) // 122
+// console.log("1" + 2)     // 12
+// console.log(1 + 2 + "2") // 32
+
