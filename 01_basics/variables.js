0d521e650ba53f84c2ee2c3560e9c1efe799d7ef
RAQIM FRAZ <79056198+raqimfraz09@users.noreply.github.com>
2024-05-30 06:17:37 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "raqim20@gmail.com"
+var accountPass = "12345"
+accountCity = "Noida"
+
+// accountId = 2  Cannot change.
+// {}  This is called as scope.
+
+accountEmail = "fraz@gmail.com"
+accountPass = "2345678"
+accountCity = "Patna"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity]) //To print in tabular structure.
