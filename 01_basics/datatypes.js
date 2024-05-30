0a7a1e7f6d70b3f6e6b61b5877742d1f53390ec8
RAQIM FRAZ <79056198+raqimfraz09@users.noreply.github.com>
2024-05-30 12:14:09 +0000

    cotainer config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // Treat all js code as a newer version.
+
+// alert(3 + 3) To use in Node it has a different syntax.
+
+// Data Types 
+
+let name = "Raqim"  // String
+let age = 22       // Int
+let loogedIn = false // Boolean
+
+// Bigint , for larger number
+// null , standalone value
+// undefined , value is not assigned
+
+let state; // undefined
+
+console.log(typeof "Raqim");
+
+console.log(typeof null);
