bfbfe30f4aeda9bc35552c02299e67e00b9a48bd
RAQIM FRAZ <79056198+raqimfraz09@users.noreply.github.com>
2024-05-30 12:44:08 +0000

    container config done

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "33abc"   // 1= 33, 2= "33", 3= "33abc" , if value is null , if undefined 
+
+// console.log(typeof(score));  // String      // for null, output will be object  // output will be undefined
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // Number    
+//console.log(valueInNumber);  // NaN , Not a Number   // 0  // NaN
+
+
+
+let isLoggedIn = "Raqim"                              // 1 gives true, "" empty gives false // "Raqim" string gives true 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber); // 44
+console.log(typeof stringNumber); // String
