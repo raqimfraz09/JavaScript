a36b16f30c2bf3746717fcdb1e41ca50abf500cb
RAQIM FRAZ <79056198+raqimfraz09@users.noreply.github.com>
2024-06-01 18:57:25 +0000

    container config done

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,35 @@
+ "use strict";
+
+// const score = 400
+// console.log(score);  // 400
+
+
+// const balance = new Number(100)
+// console.log(balance);  //  [Number: 100]
+
+// console.log(balance.toString().length); // it gives 100 but type is string
+// console.log(balance.toFixed(1));   // 100.0
+
+
+// const otherNumber = 123.8966    // 124
+// console.log(otherNumber.toPrecision(3)); //  for "23.8966"  it gives 23.9
+
+// const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));   //1,000,000   // in indian value 10,00,000
+
+ //  MATHS  
+    //  It consist of many methods
+// console.log(Math);
+
+// console.log(Math.abs(-4));  // abs gives absolute value, convert minus value into plus.
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// // we mostly use round.
+
+// console.log(Math.min(4, 3, 5, 6, 7, 1))
+
+console.log(Math.random());
+console.log(Math.random()*10);
