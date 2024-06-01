88062e4742b644c07771ea357c52bdc0428985a7
RAQIM FRAZ <79056198+raqimfraz09@users.noreply.github.com>
2024-06-01 12:59:03 +0000

    container config done

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const score = 100 // Number
+const scoreValue = 100.3  //float
+
+const isLoggedIn = false  // Boolean
+const temp = null // null
+let userEmail;  // undefined
+
+const id = Symbol('123')   // Suymbol
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+// Array, objects, functions
+
+const friend = ["raqim", "faiz", "ibshar", "zaid"];
+let myObj = {
+    name: "raqim",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof friend);
+console.log(typeof id);
+console.log(typeof myObj);
+console.log(typeof myFunction);
