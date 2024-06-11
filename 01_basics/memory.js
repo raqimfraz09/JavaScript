c4a7531efe2449d23e768ac6e74fea0bba629c45
RAQIM FRAZ <79056198+raqimfraz09@users.noreply.github.com>
2024-06-11 18:33:05 +0000

    container config done

--- 01_basics/memory.js
+++ 01_basics/memory.js
@@ -3,13 +3,16 @@
 // Stack (Primitive Data Type use stack memory).
 // Heap  (Non-Primitive uses heap memory)
 
-;let myName = "RaqimFraz"
+let myName = "RaqimFraz"
 
 let anotherName = myName
 anotherName = "FrazRaqim"
 
 console.log(myName);
 console.log(anotherName);
+
+//  In the above code "anotherName" takes the value of "myName" but it will not give you the actual value it will gives you a copy of itself.
+// When assigning a primitive value (like a string) to another variable, a copy is made. Changing the new variable does not affect the original one.
 
 let user1 = {
     email: "raqim20@gmail.com",
