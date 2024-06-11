ca018263c37a54c9054345dc8c27905a66a3a8c9
RAQIM FRAZ <79056198+raqimfraz09@users.noreply.github.com>
2024-06-11 18:46:25 +0000

    container config done

--- 01_basics/memory.js
+++ 01_basics/memory.js
@@ -25,3 +25,8 @@ user2.email = "fraz@123gmail.com"
 console.log(user1.email);
 console.log(user2.email);
 
+// When we are creating an object in JavaScript like 'user1' we are creating reference to that object in memory.
+// At this point 'user1' points to an object in memory with properties 'email' and 'upi'.
+// When we  assign "user1 to user2" we are not creating a new object, instead we are copying the reference stored in 'user1' to 'user2", Now both points to the same object.
+// Both points to same object. After changing 'user2.email' the 'email' property is updated, that's why any changes made through one reference visible through other reference as well.
+// The value of user2.email overrides the value of user1.email because user1 and user2 are both references to the same object. Modifying the object through either reference affects the object itself, and thus both references reflect the updated state of the object.
