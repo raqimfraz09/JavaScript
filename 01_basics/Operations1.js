756ccee4ce542fc9a681c541cc5ff3a47e18a5bf
RAQIM FRAZ <79056198+raqimfraz09@users.noreply.github.com>
2024-06-13 13:24:12 +0000

    container config  done

--- 01_basics/Operations1.js
+++ 01_basics/Operations1.js
@@ -4,12 +4,12 @@ let value = 3
 let negValue = -value
 console.log(negValue)
 
-// console.log(2+2)
-// console.log(2-2)
-// console.log(2*2)
-// console.log(2**2)
-// console.log(2/3)
-// console.log(2%3)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/3)
+console.log(2%3)
 
 
 
@@ -18,11 +18,16 @@ let str1 = "hello"
 let str2 = " world"
 
 let str3 = str1 + str2
-// console.log(str3)
+console.log(str3)
+// This topic is called Type Coercion.
+console.log("1" + 2)     // 12  
+console.log(1 + "2")     // 12   // JavaScript must decide whether to convert the string to a number or the number to a string. For the + operator, JavaScript prefers string concatenation when one of the operands is a string.
+console.log("1" + 2 + 2) // 122
+console.log("1" + 2)     // 12
+console.log(1 + 2 + "2") // 32
 
-// console.log("1" + 2)     // 12
-// console.log(1 + "2")     // 12
-// console.log("1" + 2 + 2) // 122
-// console.log("1" + 2)     // 12
-// console.log(1 + 2 + "2") // 32
+// When the + operator is used between two numbers, it performs addition.
+// When the + operator is used between a number and a string, it converts the number to a string and concatenates.
+// TYPE COERCION:- A process in which language automatically converts value from one data type to another.
+// This can happen implicitly(automatically) or explicitly(manually).
 
