4637ade7a27584cf80c5a543ee22a2af37c3212d
RaqimFraz09 <79056198+raqimfraz09@users.noreply.github.com>
2025-02-03 16:15:35 +0530

    New Changes

--- Code1/RevString.js
+++ Code1/RevString.js
@@ -13,3 +13,25 @@ function RevString(str){
     return reverse;
 } 
 console.log(RevString("RAQIM"));
+
+
+// String Reverse Using Array Method:-
+
+function RevString(str) {
+    let strArray = []; // Manually creating an array
+    let reverse = '';
+
+    // Storing characters in an array
+    for (let i = 0; i < str.length; i++) {
+        strArray.push(str[i]);
+    }
+
+    // Reversing the string using the array
+    for (let i = strArray.length - 1; i >= 0; i--) {
+        reverse += strArray[i];
+    }
+
+    return reverse;
+}
+
+console.log(RevString("RAQIM")); // Output: "MIQAR"
