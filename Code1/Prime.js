15ebffa42fbc8ee06b905d761c74fff2c4ace260
RaqimFraz09 <79056198+raqimfraz09@users.noreply.github.com>
2025-03-17 19:50:34 +0530

    New Changes

--- Code1/Prime.js
+++ Code1/Prime.js
@@ -10,10 +10,12 @@ function isPrime(n){
     }
     return true;
 }
-// console.log(isPrime(2));
+// console.log(isPrime(81));
+// If we want to print all prime numbers between 0 and 100. We have to right below code along with above function.
 
 for(let num=1; num<=100; num++){
     if(isPrime(num)){
         console.log(num);
     }
 }
+
