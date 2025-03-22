096c74b62de3fc48c1d41eedd2d8b99c722610da
RaqimFraz09 <79056198+raqimfraz09@users.noreply.github.com>
2025-03-22 20:55:18 +0530

    New Changes

---
+++ Code1/Anagram.js
@@ -0,0 +1,36 @@
+function Anagram(s, t){
+    s = s.split('').sort();
+    t = t.split('').sort();
+    if(s.length !== t.length){
+        return false;
+    }
+
+    for(let i = 0; i<s.length-1; i++){
+        if(s[i] !== t[i]){
+            return false;
+        }
+    }
+    return true;
+
+}
+console.log(Anagram("silent", "listen")); 
+
+// Using While Loop
+function Anagram(s, t){
+    s = s.split('').sort();
+    t = t.split('').sort();
+    if(s.length !== t.length){
+        return false;
+    }
+
+    let i = 0;
+    while(i < s.length){
+        if(s[i] !== t[i]){
+            return false;
+        }
+        i++;
+    }
+    return true;
+
+}
+console.log(Anagram("silent", "listen")); 
