c0914629a826d3e66614abe20d5c044a58970273
RaqimFraz09 <79056198+raqimfraz09@users.noreply.github.com>
2025-03-23 23:52:38 +0530

    new changes

--- Code1/Swap.js
+++ Code1/Swap.js
@@ -3,9 +3,11 @@ let b = 60;
 
 console.log("Before Swapping: a = " + a + ", b = " + b);
 
-a = a + b;
-b = a - b;
-a = a - b;
+// a = a + b;
+// b = a - b;
+// a = a - b;
 
 // console.log("After Swapping: a = " + a + ", b = " + b);
 
+[a, b] = [b, a];
+console.log("After Swapping: a = " + a + ", b = " + b);
