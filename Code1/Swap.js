2e85912e71b24a2587a8479a4310b01d5872eaee
RaqimFraz09 <79056198+raqimfraz09@users.noreply.github.com>
2025-03-24 00:05:25 +0530

    new changes

--- Code1/Swap.js
+++ Code1/Swap.js
@@ -1,4 +1,4 @@
-let a =50;
+let a = 30;
 let b = 60;
 
 console.log("Before Swapping: a = " + a + ", b = " + b);
@@ -9,5 +9,21 @@ console.log("Before Swapping: a = " + a + ", b = " + b);
 
 // console.log("After Swapping: a = " + a + ", b = " + b);
 
-[a, b] = [b, a];
+// [a, b] = [b, a];
+// console.log("After Swapping: a = " + a + ", b = " + b);
+
+// let temp = 0;
+
+// temp = a;
+// a = b;
+// b = temp;
+
+// console.log("After Swapping: a = " + a + ", b = " + b);
+
+a = a^b;
+b = a^b;
+a = a^b;
+
 console.log("After Swapping: a = " + a + ", b = " + b);
+
+
