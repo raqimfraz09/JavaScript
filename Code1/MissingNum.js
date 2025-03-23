88e19065435cad51f37a6812000fa3b43f0c9dc5
RaqimFraz09 <79056198+raqimfraz09@users.noreply.github.com>
2025-03-23 23:15:28 +0530

    new changes

--- Code1/MissingNum.js
+++ Code1/MissingNum.js
@@ -8,3 +8,19 @@ function MissingNum(n){
 }
 console.log(MissingNum([3, 0, 1]));
 console.log(MissingNum([9, 6, 4, 2, 3, 5, 7, 0, 1]));
+
+//  Using While Loop
+
+function MissingNum(n){
+    let expectedSum = n.length*(n.length+1)/2;
+    let actualSum = 0;
+
+    let i = 0;
+    while(i < n.length){
+        actualSum += n[i];
+        i++;
+    }
+    return expectedSum - actualSum
+}
+console.log(MissingNum([3, 0, 1])); 
+console.log(MissingNum([9, 6, 4, 2, 3, 5, 7, 0, 1]));
